6b47fe670bbe5fd509acd3cfd1e372e0d5279fa2
Jason Barr <jason@jasonsbarr.com>
2019-11-17 14:03:45 -0600

    Add header w/ nav to routes, set up TodoContext

--- reducer-todo/src/components/App.js
+++ reducer-todo/src/components/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Route } from "react-router-dom";
+import TodoContext from "../contexts/TodoContext";
+import reducer from "../reducers/todos";
 import { TodoList, TodoAdd } from "./Todos";
+import Header from "./Header";
 
 const App = () => {
+  const [todo, dispatch] = useReducer(reducer, {});
+
   return (
     <div className="App">
-      <Route exact path="/" component={TodoList} />
-      <Route path="todo-add" component={TodoAdd} />
+      <Header />
       {/* App controls:
-        Form w/ submit
-        Filter: all, open, done
-        Filter all, tag
-        Clear done
-        Order by: alpha, created, due, priority
-       */}
+      Form w/ submit
+      Filter: all, open, done
+      Filter all, tag
+      Clear done
+      Order by: alpha, created, due, priority
+    */}
+      <Route exact path="/" component={TodoList} />
+      <TodoContext.Provider value={todo}>
+        {/* TodoItem */}
+        {/* TodoEdit */}
+        <Route path="/todo/add" component={TodoAdd} />
+      </TodoContext.Provider>
     </div>
   );
 };
---
+++ reducer-todo/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
+
+const Header = () => {
+  const history = useHistory();
+  const { pathname } = useLocation();
+
+  return (
+    <header>
+      <Link to="/">Todo List</Link>
+      <nav>
+        {pathname !== "/todo/add" && (
+          <button onClick={() => history.push("/todo-add")}>+</button>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
