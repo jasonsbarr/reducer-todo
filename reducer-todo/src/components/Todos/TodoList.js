c338b32a83a71fee0d4d02c6508ec2b024a04356
Jason Barr <jason@jasonsbarr.com>
2019-11-18 12:56:04 -0600

    Add ability to delete todos

--- reducer-todo/src/components/Todos/TodoList.js
+++ reducer-todo/src/components/Todos/TodoList.js
@@ -5,6 +5,7 @@ import {
   TOGGLE_TODO_COMPLETE,
   CLEAR_COMPLETED_TODOS,
   CLEAR_ALL_TODOS,
+  DELETE_TODO,
 } from "../../actions/todos";
 import useTodos from "../../hooks/useTodos";
 import { TodoItem } from ".";
@@ -18,7 +19,11 @@ const TodoList = ({ todos }) => {
   const [message, setMessage] = useState("Loading...");
 
   const handleCompleteTodo = todo => {
-    const changed = { ...todo, completed: !todo.completed };
+    const changed = {
+      ...todo,
+      completed: !todo.completed,
+      updated_at: new Date(Date.now()),
+    };
     const options = {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
@@ -28,9 +33,19 @@ const TodoList = ({ todos }) => {
 
     dispatch({ type: TOGGLE_TODO_COMPLETE, payload: todo });
 
-    Axios(options)
-      .then(res => console.log(res.data))
-      .catch(err => console.error(err));
+    Axios(options).catch(err => console.error(err));
+  };
+
+  const handleDeleteTodo = todo => {
+    dispatch({ type: DELETE_TODO, payload: todo.id });
+
+    const options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      url: `${apiUrl}/${todo.id}`,
+    };
+
+    Axios(options).catch(err => console.error(err));
   };
 
   const handleClearCompletedTodos = () =>
@@ -63,6 +78,7 @@ const TodoList = ({ todos }) => {
             {state.todos.map(todo => (
               <TodoItem
                 onCompleteTodo={handleCompleteTodo}
+                onDeleteTodo={handleDeleteTodo}
                 key={todo.uuid}
                 todo={todo}
               />
